777a5d523e50dc67ac960dbeb4c34adc0e4546b1
masaftic <yousefalaa618y@gmail.com>
2023-09-25 23:05:53 +0300

    main repl, semi scanner

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler"
 #include "debug.h"
 #include "vm.h"
 
@@ -35,7 +36,8 @@ Value pop()
 
 
 
-static InterpretResult run() {
+static InterpretResult run() 
+{
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define BINARY_OP(op) \
@@ -95,10 +97,9 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
 
